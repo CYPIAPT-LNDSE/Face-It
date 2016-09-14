f6fc7698f33dcf8e1ff822b5a0e9ea1a854b3fd6
mattiaS <mattia@3doers.it>
2016-09-14 11:56:18 +0100

    add gameSetGenerator

--- js/helpers.js
+++ js/helpers.js
@@ -16,31 +16,41 @@ function showFromLeft(elementToShow){
   }
 }
 
-function generateGameSet(){
+function generateGameSet(images){
   //populate a gameset []
+  return randomiser(images).map(function(el, i, array){
+    return {
+      [el]:images[el][0],
+      userGuess:'noresponse'
+    }
+  })
 }
 
+function randomiser(images){
+  return Object.keys(images).sort(function(){return .5 - Math.random()}).slice(0, 5)  
+}
 
-function instanceImageObj(){
-// generate new object from images in db, new property to record uses guess
+function instanceImageObj(obj){
+  // generate new object from images in db, new property to record uses guess
+  return obj.userGuess = 'noresponse'
 }
 
 function generateGameDOMElements(gameset){
-//generate all pages of the game
-//return 
+  //generate all pages of the game
+  //return 
 }
 
 function generateSingleGamePage(){
-//generate single div of game
+  //generate single div of game
 }
 
 function measureOutOfView(){
-//return position where to place the hidden dom elements
+  //return position where to place the hidden dom elements
 }
 
 
 function renderElements(hiddenXRef){
-//render elemets out of view
+  //render elemets out of view
 }
 
 
