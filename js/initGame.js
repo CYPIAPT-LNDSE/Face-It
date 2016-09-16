7304feb5bee996e0380086a876afd771e04800cf
mattiaS <mattia@3doers.it>
2016-09-16 10:16:05 +0100

    apply emoji and text to btn

--- js/initGame.js
+++ js/initGame.js
@@ -1,8 +1,12 @@
 function initGame(){
   let gameSet = generateGameSet(images)
+  gameSet = generateWrongAnswerData(gameSet)
   renderHiddenDivs()
   applyGameSetToHiddenDivs(gameSet)
+  applyEmojiL(gameSet)
+  applyEmojiR(gameSet)
   eventListenerGamePage()
+  console.log(gameSet)
 }
 
 //helpers
@@ -11,9 +15,34 @@ function generateGameSet(images, username = 'john doe'){
     return {
       [el]:images[el][0],
       userGuess:'noresponse',
-      username:username
+      username:username,
+      apiGuess:apiWinner(images[el][0]['scores']),
+      correctAnswer:{
+        emotion:apiWinner(images[el][0]['scores']),
+        emojiPath: 'assets/icons/' + emojiPath(apiWinner(images[el][0]['scores'])[0])
+      }
     }
   })
+}
+
+function generateWrongAnswerData(gameSet){
+  return gameSet.map(function(el, index){
+    el.wrongAnswer = {
+      emotion:getOtherEmotion(el.correctAnswer['emotion'][0]),
+      emojiPath:'assets/icons/' + emojiPath(getOtherEmotion(el.correctAnswer['emotion'][0]))
+    }
+    return el
+  })
+}
+
+function getOtherEmotion(emotion, pos){
+  let emotions = ['happiness', 'sadness', 'anger', 'neutral','surprise']
+  emotions.splice(emotions.indexOf(emotion), 1)
+  return emotions[2]
+  
+  function randomNumber(){
+    return ~~(Math.random()*5) 
+  }
 }
 
 function randomiser(images){
@@ -28,18 +57,32 @@ function renderHiddenDivs(){
 
 }
 function applyGameSetToHiddenDivs(gameSet){
+
   //apply correct images and emoticons to each hidden div
   ['gamePage1','gamePage2','gamePage3','gamePage4', 'gamePage5'].forEach(function(el, i, array){
     $('#'+ el).find('#biggie').attr('src', 'assets/imgs/' + Object.keys( gameSet[i])[0]+ '.jpg' )
-    console.log($('#'+ el).find('img').attr('src'))
   })
+  
   //apply correct emoticons
 }
-
-function detectCorrectAnswer(img){
+function applyEmojiL(gameSet){
+['gamePage1','gamePage2','gamePage3','gamePage4', 'gamePage5'].forEach(function(el,i){
+  console.log(gameSet[i].correctAnswer.emojiPath)
+    $('#'+ el).find('#choiceL').attr('src',String(gameSet[i].correctAnswer.emojiPath))
+    $('#'+ el).find('#buttonL').html(gameSet[i].correctAnswer.emotion[0])
+  })
+}
+function applyEmojiR(gameSet){
+['gamePage1','gamePage2','gamePage3','gamePage4', 'gamePage5'].forEach(function(el,i){
+  console.log(gameSet[i].correctAnswer.emojiPath)
+    $('#'+ el).find('#choiceR').attr('src',String(gameSet[i].wrongAnswer.emojiPath))
+    $('#'+ el).find('#buttonR').html(gameSet[i].wrongAnswer.emotion)
+  })
+}
+function emojiPath(emotion){
   let dict = {
-    'angry':'angry-emoji.svg', 'sad':'sad-emoji.svg', 'surprise':'surprise-emoji.svg', 'neutral':'neutral-emoji.svg', 'happy':'happy-emoji.svg'}
-
+    'anger':'angry-emoji.svg', 'sadness':'sad-emoji.svg', 'surprise':'surprise-emoji.svg', 'neutral':'neutral-emoji.svg', 'happiness':'happy-emoji.svg'}
+  return dict[emotion]
 }
 function eventListenerGamePage(){
   [1,2,3,4,5].forEach(function(el){
@@ -56,4 +99,23 @@ function eventListenerGamePage(){
       })
     }
   })
+}
+
+function apiWinner(scores) {
+  let tempNumber = 0;
+  let emotion;
+  for(prop in scores){
+    if(scores[prop] > tempNumber && String(scores[prop]).includes('-') === false){
+      tempNumber = scores[prop];
+      emotion = prop;
+    }
+  }
+  return [emotion, tempNumber];
+}
+let dictionary = {
+  happiness:'happy-emoji.svg',
+  sadness:'sad-emoji.svg',
+  neutral:'neutral-emoji.svg',
+  surprise:'surprise-emoji.svg',
+  anger:'angry-emoji.svg'
 }
