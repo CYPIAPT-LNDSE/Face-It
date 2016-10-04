3d78412c3042a8cd7714f7fdc0ff8dbcfd940ad4
jakub <jsvihla@gmail.com>
2016-10-04 16:26:50 +0100

    adds functionality to buttons in navigator

--- js/loader.js
+++ js/loader.js
@@ -1,6 +1,7 @@
 function loader(){
   $('#main').append(pages['landing'])
   $('#main').append(pages['intro'])
+  $('#main').append(pages['navigator'])
 
   $('#landing').show("slide", { direction: "left" }, 500)
   $('#start').click(function(){
@@ -10,11 +11,23 @@ function loader(){
     $('#intro').show("slide", { direction: "left" }, 500)
   })
 
-  $('#introStart').click(function(){
+  $('#continue_button').click(function(){
     initGame()
     $('#intro').hide()
+    var $nav = $('#navigator').hide();
+    $('#navigator').show("slide", { direction: "left" }, 500) //sliding does not work
+  })
+
+  $('#start_game_button').click(function(){
+    $('#navigator').hide()
     $('#gamePage1').show("slide", { direction: "left" }, 500)
   })
+
+  $('#lifetime_results_button').click(function(){
+    $('#navigator').hide()
+    $('#lifeTimePage').show("slide", { direction: "left" }, 500)
+  })
+
 }
 
 $(document).ready(loader)
