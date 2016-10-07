0b9d8931d3fbc63023ca5cf9f698aa38c67329f1
heatherlauren <heatherlaurendavidson@gmail.com>
2016-10-07 12:50:45 +0100

    add data points to line chart

--- js/lifetime-graph.js
+++ js/lifetime-graph.js
@@ -1,12 +1,15 @@
-const w = 500;
-const h = 200;
-var path;
+var margin = {top: 10, right: 10, bottom: 10, left: 10},
+    width = 400 - margin.left - margin.right,
+    height = 200 - margin.top - margin.bottom
 var roundResults;
 
 d3.json('../dummyData/roundResults.json', function (error, results) {
 
   if (error) return console.warn(error);
+
   roundResults = results.roundResults;
+
+  var dates = roundResults.map(function(a) {return a.date});
 
   // roundResults.forEach(function (d) {
   //   var formatTime = d3.timeFormat("%d/%m");
@@ -14,24 +17,15 @@ d3.json('../dummyData/roundResults.json', function (error, results) {
   //   console.log(d.date)
   // });
 
-  path = d3
-    .select('#lifetime-results-page__lifetime-graph')
-    .append('svg')
-    .attr('width', '100%')
-    .attr('height', '100%')
-    .append('g')
-    .append('path')
-
-  var dates = roundResults.map(function(a) {return a.date});
-  var scores = roundResults.map(function(a) {return a.score});
+  // Create line chart
 
   var x = d3.scaleTime()
     .domain(d3.extent(dates))
-    .range([0, w]);
+    .range([0, width]);
 
   var y = d3.scaleLinear()
     .domain([0, 100])
-    .range([h, 0]);
+    .range([height, 0]);
 
   var line = d3.line()
     .x(function (d) {
@@ -41,8 +35,25 @@ d3.json('../dummyData/roundResults.json', function (error, results) {
       return y(d.score);
     })
 
-  path
+  var svg = d3
+    .select("#lifetime-results-page__lifetime-graph")
+    .append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+      .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+
+  svg.append('path')
     .datum(roundResults)
     .attr('d', line);
+
+  svg.selectAll('dot')
+      .data(roundResults)
+    .enter().append('circle')
+      .attr('class', 'lifetime-graph__dots')
+      .attr('r', 8)
+      .attr("cx", function(d) { return x(d.date); })
+      .attr("cy", function(d) { return y(d.score); });
 
 });
