4c729af6a583623624321f1087371d53fcd478a6
heatherlauren <heatherlaurendavidson@gmail.com>
2016-10-07 16:26:41 +0100

    lifetime results graph panning

--- js/lifetime-graph.js
+++ js/lifetime-graph.js
@@ -3,7 +3,7 @@ var margin = {top: 10, right: 10, bottom: 10, left: 10},
     height = 200 - margin.top - margin.bottom
 var roundResults;
 
-d3.json('../dummyData/roundResults.json', function (error, results) {
+d3.json("../dummyData/roundResults.json", function (error, results) {
 
   if (error) return console.warn(error);
 
@@ -17,43 +17,61 @@ d3.json('../dummyData/roundResults.json', function (error, results) {
   //   console.log(d.date)
   // });
 
-  // Create line chart
-
-  var x = d3.scaleTime()
+  var xScale = d3.scaleTime()
     .domain(d3.extent(dates))
     .range([0, width]);
 
-  var y = d3.scaleLinear()
+  var yScale = d3.scaleLinear()
     .domain([0, 100])
     .range([height, 0]);
 
   var line = d3.line()
     .x(function (d) {
-      return x(d.date);
+      return xScale(d.date);
     })
     .y(function (d) {
-      return y(d.score);
+      return yScale(d.score);
     })
+
+
+  // Create zoom event
+
+  var zoom = d3.zoom()
+    .scaleExtent([1, 1])
+    .translateExtent([[-20, -20], [width + 20, height + 20]])
+    .on("zoom", zoomed);
+
+  function zoomed() {
+    svg.attr(
+      "transform",
+      `translate(${d3.event.transform.x}, 0)`
+    )}
+
+  // Create chart instance
 
   var svg = d3
     .select("#lifetime-results-page__lifetime-graph")
-    .append('svg')
-      .attr('width', width + margin.left + margin.right)
-      .attr('height', height + margin.top + margin.bottom)
-    .append('g')
-      .attr("transform", `translate(${margin.left}, ${margin.top})`);
+    .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .call(zoom)
+    .append("g")
+      .attr("transform", `translate(${margin.left},${margin.top},${margin.right},${margin.bottom})`)
 
+  // Add line chart
 
-  svg.append('path')
+  svg.append("path")
     .datum(roundResults)
-    .attr('d', line);
+    .attr("d", line);
 
-  svg.selectAll('dot')
+  // Create data points on line chart
+
+  svg.selectAll("dot")
       .data(roundResults)
-    .enter().append('circle')
-      .attr('class', 'lifetime-graph__dots')
-      .attr('r', 8)
-      .attr("cx", function(d) { return x(d.date); })
-      .attr("cy", function(d) { return y(d.score); });
+    .enter().append("circle")
+      .attr("class", "lifetime-graph__dots")
+      .attr("r", 8)
+      .attr("cx", function(d) { return xScale(d.date); })
+      .attr("cy", function(d) { return yScale(d.score); });
 
 });
