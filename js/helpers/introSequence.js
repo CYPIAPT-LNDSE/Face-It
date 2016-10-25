fa62c47a7f1b21a1cc829c045b31b1548a101e3e
heatherlauren <heatherlaurendavidson@gmail.com>
2016-10-25 17:05:49 +0100

    add transitions for speech bubble

--- js/helpers/introSequence.js
+++ js/helpers/introSequence.js
@@ -66,18 +66,46 @@ function userIconAnimation() {
     .velocity({translateY:219}, 80, [0.33, 0, 0.66, 1])
     .velocity({translateY:218}, 100, [0.33, 0, 0.66, 1])
     .velocity({translateY:219}, 80, [0.33, 0, 0.66, 1])
-    // Scale
-    .velocity({scale: "400%"}, {delay: 400, duration: 800})
+    // Scale, hide conveyer belts, and display speech bubble
+  $('#user-icon')
+    .velocity({scale: "400%"}, {delay: 400, duration: 800, begin: function() {
+        $('#conveyer-group').velocity({display: "none", opacity: 0})
+        $('.intro-page__speech-bubble').velocity({opacity: 1}, {delay: 2000, display: "block", complete: function() {
+          speechBubble()
+        }})
+      }
+    })
     // Fall down
     .velocity({translateY:450}, 600, [0.33, 0, 0.66, 0.33])
     .velocity({translateY:445}, 100, [0.33, 0, 0.66, 1])
     .velocity({translateY:450}, 80, [0.33, 0, 0.66, 1])
     .velocity({translateY:447}, 100, [0.33, 0, 0.66, 1])
     .velocity({translateY:450}, 80, [0.33, 0, 0.66, 1])
-  $('#conveyer-group')
-    .velocity({display: "none", opacity: 0}, {delay: 1400})
-  $('.intro-page__speech-bubble')
-    .velocity({opacity: 1}, {delay: 3800})
+}
+
+function speechBubble() {
+  $(document).on("click", function() {
+    var speechBubble = $(".intro-page__speech-bubble");
+    if (speechBubble.hasClass("intro-page__speech-bubble--1")) {
+      $("span").text("I was built to recognise emotions")
+      $(speechBubble).switchClass("intro-page__speech-bubble--1", "intro-page__speech-bubble--2")
+    }
+    else if (speechBubble.hasClass("intro-page__speech-bubble--2")) {
+      $("span").text("But I need some practice before I'm ready to leave the factory")
+      $(speechBubble).switchClass("intro-page__speech-bubble--2", "intro-page__speech-bubble--3")
+    }
+    else if (speechBubble.hasClass("intro-page__speech-bubble--3")) {
+      $("span").text("Can you help me practice recognising emotions?")
+      $(speechBubble).removeClass("intro-page__speech-bubble--3")
+    }
+    else {
+      tutorial()
+    }
+  })
+}
+
+function tutorial() {
+  console.log("tutorial")
 }
 
 surpriseIconAnimation()
