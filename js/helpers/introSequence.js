9551a66bb63b39149c9c6ecfdd900261abd0c58b
heatherlauren <heatherlaurendavidson@gmail.com>
2016-10-27 17:46:35 +0100

    add js for intro sequence tutorial and second speech bubble stage

--- js/helpers/introSequence.js
+++ js/helpers/introSequence.js
@@ -1,5 +1,5 @@
 function surpriseIconAnimation() {
-  $('#surprise-icon')
+  $("#surprise-icon")
     // Fall down
     .velocity({translateX:[10,11], translateY:[219, 60]}, 800, [0.33, 0, 0.66, 0.33])
     .velocity({translateY:216}, 100, [0.33, 0, 0.66, 1])
@@ -29,7 +29,7 @@ function surpriseIconAnimation() {
 }
 
 function sadIconAnimation() {
-  $('#sad-icon')
+  $("#sad-icon")
     // Fall down
     .velocity({translateX:[10,11], translateY:[219, 60]}, 800, [0.33, 0, 0.66, 0.33])
     .velocity({translateY:216}, 100, [0.33, 0, 0.66, 1])
@@ -59,7 +59,7 @@ function sadIconAnimation() {
 }
 
 function userIconAnimation() {
-  $('#user-icon')
+  $("#user-icon")
     // Fall down
     .velocity({translateX:[10,11], translateY:[219, 60]}, 800, [0.33, 0, 0.66, 0.33])
     .velocity({translateY:216}, 100, [0.33, 0, 0.66, 1])
@@ -67,11 +67,11 @@ function userIconAnimation() {
     .velocity({translateY:218}, 100, [0.33, 0, 0.66, 1])
     .velocity({translateY:219}, 80, [0.33, 0, 0.66, 1])
     // Scale, hide conveyer belts, and display speech bubble
-  $('#user-icon')
+  $("#user-icon")
     .velocity({scale: "400%"}, {delay: 400, duration: 800, begin: function() {
-        $('#conveyer-group').velocity({display: "none", opacity: 0})
-        $('.intro-page__speech-bubble').velocity({opacity: 1}, {delay: 2000, display: "block", complete: function() {
-          speechBubble()
+        $("#conveyer-group").velocity({opacity: 0})
+        $(".intro-page__speech-bubble").velocity({opacity: 1}, {delay: 2000, display: "block", complete: function() {
+          speechBubbleText()
         }})
       }
     })
@@ -83,29 +83,90 @@ function userIconAnimation() {
     .velocity({translateY:450}, 80, [0.33, 0, 0.66, 1])
 }
 
-function speechBubble() {
+function speechBubbleText() {
   $(document).on("click", function() {
     var speechBubble = $(".intro-page__speech-bubble");
-    if (speechBubble.hasClass("intro-page__speech-bubble--1")) {
-      $("span").text("I was built to recognise emotions")
-      $(speechBubble).switchClass("intro-page__speech-bubble--1", "intro-page__speech-bubble--2")
-    }
-    else if (speechBubble.hasClass("intro-page__speech-bubble--2")) {
-      $("span").text("But I need some practice before I'm ready to leave the factory")
-      $(speechBubble).switchClass("intro-page__speech-bubble--2", "intro-page__speech-bubble--3")
-    }
-    else if (speechBubble.hasClass("intro-page__speech-bubble--3")) {
-      $("span").text("Can you help me practice recognising emotions?")
-      $(speechBubble).removeClass("intro-page__speech-bubble--3")
-    }
-    else {
-      tutorial()
+    switch (true) {
+      case $(speechBubble).hasClass("intro-page__speech-bubble--1"):
+        $(".speech-bubble__text").text("I was built to recognise emotions")
+        $(speechBubble).switchClass("intro-page__speech-bubble--1", "intro-page__speech-bubble--2")
+        break;
+      case $(speechBubble).hasClass("intro-page__speech-bubble--2"):
+        $(".speech-bubble__text").text("But I need some practice before I'm ready to leave the factory")
+        $(speechBubble).switchClass("intro-page__speech-bubble--2", "intro-page__speech-bubble--3")
+        break;
+      case $(speechBubble).hasClass("intro-page__speech-bubble--3"):
+        $(".speech-bubble__text").text("Can you help me practice recognising emotions?")
+        $(speechBubble).switchClass("intro-page__speech-bubble--3", "intro-page__speech-bubble--4")
+        break;
+      case $(speechBubble).hasClass("intro-page__speech-bubble--4"):
+        $(document).off();
+        $(speechBubble).switchClass("intro-page__speech-bubble--4", "intro-page__speech-bubble--5")
+        tutorialFace()
+        break
+      case $(speechBubble).hasClass("intro-page__speech-bubble--5"):
+        $(".speech-bubble__text").text("Play enough rounds, and I will level up!")
+        $(speechBubble).switchClass("intro-page__speech-bubble--5", "intro-page__speech-bubble--6")
+        break
+      case $(speechBubble).hasClass("intro-page__speech-bubble--6"):
+        $(".speech-bubble__text").text("Help me level up and travel through the factory")
+        $(speechBubble).switchClass("intro-page__speech-bubble--6", "intro-page__speech-bubble--7")
+        break
+      case $(speechBubble).hasClass("intro-page__speech-bubble--7"):
+        $(".speech-bubble__text").velocity({opacity: 0}, {complete: function() {
+          $(".speech-bubble__text").velocity({"font-size": "1.8em"})
+          $(".speech-bubble__text").text("Let's go!")
+          $(".speech-bubble__text").velocity({opacity: 1})
+        }})
+        $(speechBubble).removeClass("intro-page__speech-bubble--7")
+        introToLevelPage()
+        break
     }
   })
 }
 
-function tutorial() {
-  console.log("tutorial")
+function tutorialFace() {
+  $(".intro-page__speech-bubble").velocity({opacity: 0}, {display: "none"})
+  $("svg").velocity({opacity: 0}, {display: "none"})
+  setTimeout(function() {
+    $(".intro-page__tutorial-button").velocity({opacity: 0.3}, {display: "block", duration: 800})
+    $(".game-page__image-container--intro").velocity({opacity: 1}, {display:"block", duration: 800, queue: false, complete: function() {
+      $(".intro-page__modal").velocity({opacity: 1}, {delay: 200, duration: 800})
+      tutorialButtons()
+    }})
+  }, 400)
+}
+
+function tutorialButtons() {
+  $(document).on("click", function() {
+    $(".game-page__image-container--intro").velocity({opacity: 0.3}, {duration: 1000})
+    $(".intro-page__modal").switchClass("intro-page__modal--top-triangle", "intro-page__modal--bottom-triangle")
+    $(".modal__text").velocity({opacity: 0}, {complete: function() {
+      $(".modal__text").text("Tap on which emotion you think it is showing")
+      $(".modal__text").velocity({opacity: 1})
+    }})
+    $(".intro-page__tutorial-button").velocity({opacity: 1}, {duration: 1000, delay: 300})
+    $(document).off()
+    tutorialtoSpeechBubble()
+  })
+}
+
+function tutorialtoSpeechBubble() {
+  $(".intro-page__tutorial-button").on("click", function() {
+    $(".speech-bubble__text").text("After five faces, I'll show you what I thought")
+    $(".intro-page__tutorial").velocity({opacity: 0}, {duration: 800, display: "none"})
+    $("svg").velocity({opacity: 1}, {display: "block", duration: 600, delay: 400})
+    $(".intro-page__speech-bubble").velocity({opacity: 1}, {display: "block", duration: 600, delay: 800})
+    $(".intro-page__tutorial-button").off()
+    speechBubbleText()
+  })
+}
+
+function introToLevelPage() {
+  $(document).on("click", function() {
+    console.log("level page")
+    $(document).off();
+  })
 }
 
 surpriseIconAnimation()
